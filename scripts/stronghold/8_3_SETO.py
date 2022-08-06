24c1db3d60d9cf0b4fc59238428232051f0df73d
EunjunChoi <universe.choi@gmail.com>
2022-08-06 23:58:28 +0900

    initial commit for 8_3_SETO

--- scripts/stronghold/8_3_SETO.py
+++ scripts/stronghold/8_3_SETO.py
@@ -1,24 +1,10 @@
-# 8_3_SETO
+# 8_6_TRIE
 
 ### read data ###
-with open("../../inputs/stronghold/rosalind_seto.txt", "r") as f:
-    n = int(f.readline().rstrip())
-    set_U = set(range(1, n + 1))
-    set_A = set(map(int, f.readline().strip("{}\n").split(", ")))
-    set_B = set(map(int, f.readline().strip("{}\n").split(", ")))
+with open("../../inputs/stronghold/rosalind_trie.txt", "r") as f:
+    seqs = []
+    for line in f:
+        seq = line.rstrip()
+        seqs.append(seq)
 
-### set operations ###
-union = set_A.union(set_B)
-intersection = set_A.intersection(set_B)
-A_diff = set_A.difference(set_B)
-B_diff = set_B.difference(set_A)
-A_comp = set_U.difference(set_A)
-B_comp = set_U.difference(set_B)
-
-### print ###
-print(union)
-print(intersection)
-print(A_diff)
-print(B_diff)
-print(A_comp)
-print(B_comp)
+print(seqs)
