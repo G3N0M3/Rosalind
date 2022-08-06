481e76744e05f8d13b07c6420eb0a00fa4933717
EunjunChoi <universe.choi@gmail.com>
2022-08-06 22:59:00 +0900

    adds solution for 8_2

--- scripts/stronghold/8_2_ASPC.py
+++ scripts/stronghold/8_2_ASPC.py
@@ -1,6 +1,17 @@
-# 7_1_SIGN
-import scripts.Rosalind as rs
+# 8_2_ASPC
+from math import comb
 
 ### read data ###
 with open("../../inputs/stronghold/rosalind_aspc.txt", "r") as f:
-    pass
+    n, m = map(int, f.readline().rstrip().split())
+
+### calculation ###
+"""
+Sum(k from m to n) nCk
+"""
+res = 0
+for k in range(m, n + 1):
+    res += comb(n, k)
+    res %= 1000000
+
+print(res)
