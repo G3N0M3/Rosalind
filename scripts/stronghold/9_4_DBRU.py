871152c6487eb3f67880dc7cb8137ccfa09f1d75
EunjunChoi <universe.choi@gmail.com>
2022-08-13 22:18:15 +0900

    adds solution for 9_4_DBRU

--- scripts/stronghold/9_4_DBRU.py
+++ scripts/stronghold/9_4_DBRU.py
@@ -1,7 +1,20 @@
 # 9_4_DBRU
+import scripts.Rosalind as rs
 
 ### read data ###
 with open("../../inputs/stronghold/rosalind_dbru.txt", "r") as f:
+    seq = f.readline().rstrip()
     k_plus_mers = {seq.rstrip() for seq in f.readlines()}
+    k_plus_mers = k_plus_mers | {seq}
+    k = len(seq) - 1
 
-print(k_plus_mers)
+### get reverse complement and merge ###
+k_plus_mers_reverse = {rs.reverse_complement(seq) for seq in k_plus_mers}
+k_plus_mers_total = k_plus_mers | k_plus_mers_reverse
+
+### get output list ###
+res = [(r[:k], r[1:k + 1]) for r in k_plus_mers_total]
+
+### print ###
+for item in res:
+    print(f"({item[0]}, {item[1]})")
