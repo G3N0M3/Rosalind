1e24976bb702ed9ecd5489907673a68e0277af6d
EunjunChoi <universe.choi@gmail.com>
2022-08-13 21:16:06 +0900

    adds solution for 9_3_INDC

--- scripts/stronghold/9_3_INDC.py
+++ scripts/stronghold/9_3_INDC.py
@@ -1,8 +1,16 @@
 # 9_3_INDC
-import scripts.Rosalind as rs
+from math import comb
+from math import log10
 
 ### read data ###
 with open("../../inputs/stronghold/rosalind_indc.txt", "r") as f:
     n = int(f.readline().rstrip())
 
-print(n)
+res = []
+_sum = 0
+for k in range(2 * n, 0, -1):
+    _sum += comb(2 * n, k) * (.5 ** k) * (.5 ** (2 * n - k))
+    res.append(log10(_sum))
+
+for cal in res[::-1]:
+    print(f"{cal:.4f}", end=" ")
